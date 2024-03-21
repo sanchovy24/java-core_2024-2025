2dfe96466b6eaed5249f66870ef29703c2f69c65
sanchovy24 <Ryazanova_av00@mail.ru>
2024-03-21 13:39:02 +0500

    Лабораторная работа 1

--- src/Irl/Example10.java
+++ src/Irl/Example10.java
@@ -1,17 +1,16 @@
 package Irl;
 import java.util.Scanner;
-
+import java.time.LocalDate;
 public class Example10 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.println("Введите первое число: ");
-        int number1 = in.nextInt();
-        System.out.println("Введите второе число: ");
-        int number2 = in.nextInt();
-        int sum = number1 + number2;
-        int difference = number1 - number2;
-        System.out.println("Сумма чисел: " + sum);
-        System.out.println("Разность чисел: " + difference);
+        LocalDate current_date = LocalDate.now();
+        int current_year = current_date.getYear();
+        System.out.println("Год рождения пользователя: ");
+        int birth_year = in.nextInt();
+        int age = current_year - birth_year;
+        System.out.println("Возраст пользователя: " + age);
         in.close();
     }
 }
+
--- src/Main.java
+++
@@ -1,5 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        long max = 3123456789L;
-
-}}
